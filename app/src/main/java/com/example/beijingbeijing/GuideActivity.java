f7b7d225673c365cc567098f19b0440610041d8f
ChenYuelun <911172573@qq.com>
2017-06-02 12:48:03 +0800

    引导页面内容填充

--- app/src/main/java/com/example/beijingbeijing/GuideActivity.java
+++ app/src/main/java/com/example/beijingbeijing/GuideActivity.java
@@ -1,11 +1,17 @@
 package com.example.beijingbeijing;
 
 import android.os.Bundle;
+import android.support.v4.view.PagerAdapter;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.RelativeLayout;
+
+import java.util.ArrayList;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -22,14 +28,57 @@ public class GuideActivity extends AppCompatActivity {
     @BindView(R.id.activity_guide)
     RelativeLayout activityGuide;
 
+    private ArrayList<ImageView> imageViews;
+    private int[] ids = {R.drawable.guide_1,R.drawable.guide_2,R.drawable.guide_3};
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_guide);
         ButterKnife.bind(this);
+        initData();
+
+        viewpager.setAdapter(new MyPagerAdapter() );
+    }
+
+    private void initData() {
+        imageViews = new ArrayList<>();
+        for (int i=0;i<ids.length;i++){
+            ImageView imageView = new ImageView(this);
+            //注意要设置背景
+            imageView.setBackgroundResource(ids[i]);
+
+            //添加到集合中
+            imageViews.add(imageView);
+        }
     }
 
     @OnClick(R.id.bt_star_main)
     public void onViewClicked() {
+    }
+
+    private class MyPagerAdapter extends PagerAdapter {
+
+        @Override
+        public void destroyItem(ViewGroup container, int position, Object object) {
+            container.removeView((View) object);
+        }
+
+        @Override
+        public Object instantiateItem(ViewGroup container, int position) {
+            ImageView imageView = imageViews.get(position);
+            container.addView(imageView);
+            return imageView;
+        }
+
+        @Override
+        public int getCount() {
+            return imageViews.size();
+        }
+
+        @Override
+        public boolean isViewFromObject(View view, Object object) {
+            return view == object;
+        }
     }
 }
